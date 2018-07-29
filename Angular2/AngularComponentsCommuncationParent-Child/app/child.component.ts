e58980233283cb32f949ac9742ac2b2117c81756
Amit Bhalerao <amitbaramatimca@gmail.com>
2018-07-29 16:54:10 +0530

    Angular Pass data to Child Component

---
+++ Angular2/AngularComponentsCommuncationParent-Child/app/child.component.ts
@@ -0,0 +1,12 @@
+
+import { Component, Input  } from '@angular/core';
+
+@Component({
+    selector: 'child-component',
+    template: `<h2>Child Component</h2>
+               Current count is {{ count }}
+    `
+})
+export class ChildComponent {
+    @Input() count: number;
+}
