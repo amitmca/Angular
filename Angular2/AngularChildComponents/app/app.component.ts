95fe2b9a2dd093e5b106f1716e86c11a07535330
Amit Bhalerao <amitbaramatimca@gmail.com>
2018-07-29 16:25:08 +0530

    Creating Child Components in Angular 2

---
+++ Angular2/AngularChildComponents/app/app.component.ts
@@ -0,0 +1,10 @@
+import {Component, OnInit} from '@angular/core';
+@Component({
+   templateUrl: `app/app.component.html`,
+    selector: 'my-app'
+})
+export class AppComponent {
+
+    public title : string = 'Angular Components';
+      
+}
